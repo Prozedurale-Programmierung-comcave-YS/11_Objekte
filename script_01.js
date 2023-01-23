2f7880eda65d78358b1fa8144026890561fc8a36
poisonpanicyvy <poisonpanicyvy@web.de>
2023-01-23 09:37:38 +0100

    1.versuch

--- script_01.js
+++ script_01.js
@@ -1,18 +1,30 @@
 
 /***** Erinnerung: Arrays *******/
 
-// let arr;
-// arr = ["Ich","bin","Max"];
-// output(arr);
-// output(arr[0]);
-// output("------------");
+ let arr;
+arr = ["Ich","bin","Max"];
+output(arr);
+output(arr[0]);
+
+
+output("------------");
 
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+// nested arrays - Prinzip:[[[]]]
+// nested arraysn-hier: [[],[]]
+arr =   [
+                ["Ich","bin","Max"],
+                ["Ich","bin","Moritz"]
+        ];
+        // output(arr[0][2]);
+        // output(arr[0][0]);
 
-
-
+for (let i = 0; i < arr.length; i++) {
+     output(arr[i]);
+        
+}
 
 /** Ausgabe */
 function output(outputData) {
