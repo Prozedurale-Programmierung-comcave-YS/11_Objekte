b46032a4748d9af5c96729d6aa9d7e9a896f415c
poisonpanicyvy <poisonpanicyvy@web.de>
2023-01-23 12:13:18 +0100

    new changes

--- script_01.js
+++ script_01.js
@@ -30,37 +30,56 @@
 // }
 
 
-let person = {
-firstName:"Yvonne",
-familyName:"Schubert",
-salary: [120000,160000],
-permission: true,
-sayHello:
-        function(){
-                return "Hallo ich bin " + this.firstName + "!";
+// let person = {
+// firstName:"Yvonne",
+// familyName:"Schubert",
+// salary: [120000,160000],
+// permission: true,
+// sayHello:
+//         function(){
+//                 return "Hallo ich bin " + this.firstName + "!";
+// }
+// }
+// // output(person);
+// // output(person.firstName);
+// // output(person.permission);
+// // output(person.salary[1]);
+
+// person.familyName = "Schubert";
+
+
+// const txt = "Ich bin " + person.firstName + " " +
+// person.familyName +
+//  " und verdiene " +
+// person.salary[1] + " p.a";
+
+// output(txt)
+// output(person.sayHello());
+
+// Objekt
+
+const baikal = {
+        value:"10m",
+        deep:{
+                deeper:{
+                        deepest: {
+                                value:"Das Licht - auf 1642m!",
+                                temperature:1
+                        }
+                }
+        }
+
+
+
+
 }
-}
-output(person);
-output(person.firstName);
-output(person.permission);
-output(person.salary[1]);
-
-person.familyName = "Schubert";
 
 
-const txt = "Ich bin " + person.firstName + " " +
-person.familyName +
- " und verdiene " +
-person.salary[1] + " p.a";
-
-output(txt)
-output(person.sayHello());
-
-
-
-
-
-
+output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
+output(baikal.deep.deeper.deepest.value);
+output(baikal.deep.deeper.deepest.temperature);
 
 
 
